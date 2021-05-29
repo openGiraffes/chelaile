b975dd85d437963ea5f461efc855f1fb61af1941
Ding <michael19950415@outlook.com>
2021-05-30 00:54:25 +0800

    fix location

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let cityInfo = null, lat = 0, lng = 0;
 $(function () {
     getLocation();
     document.activeElement.addEventListener('keydown', handleKeydown);
@@ -13,13 +14,12 @@ $(function () {
         var keyword = e.target.value;
         var cityName = $('#gps_city').text();
         var result = $.getApi('http://api.map.baidu.com/geocoder/v2/?address=' + keyword + '&output=json&ak=Bsr5iefxHEwQD8iCFTx3GwWOem0ZoSBk&city=' + cityName);
-        console.log(result)
+        console.log(cityName, result)
         if (result.status == 0) {
-            var lat = result.result.location.lat;
-            var lng = result.result.location.lng;
+            lat = result.result.location.lat;
+            lng = result.result.location.lng;
             $('#gps_city').text(keyword);
             $('#lctt').text(result.result.level);
-            $('#lct').val(lat + ';' + lng);
         }
     }
 });
@@ -28,10 +28,8 @@ function gotoPage() {
     var cid = $('#cid').val();
     var checked = $('#api').is(':checked');
     if (cid == '') {
-        if (checked) {
-            var arr = $('#lct').val().split(';');
-            getCityInfo(arr[0], arr[1]);
-        }
+        if (checked)
+            getCityInfo();
         else
             openWeb();
     }
@@ -47,21 +45,24 @@ function openWeb() {
     window.location.href = '../web/index.html';
 }
 
-function getCityInfo(lat, lng) {
-    var url = 'https://api.chelaile.net.cn/goocity/city!localCity.action?s=IOS&gpsAccuracy=80.000000&gpstype=wgs&push_open=1&vc=10554&lat=' + lat + '&lng=' + lng;
-    var result = $.getApi(url, 'text');
-    let data = JSON.parse(result.replace("**YGKJ", "").replace("YGKJ##", ""));
-    if (data.jsonr.status == '00') {
-        var cityId = data.jsonr.data.localCity.cityId;
-        window.location.href = '../info/index.html?cid=' + cityId + '&lat=' + lat + '&lng=' + lng;
+function getCityInfo() {
+    if (cityInfo) {
+        if (cityInfo.jsonr.status == '00') {
+            var cityId = cityInfo.jsonr.data.localCity.cityId;
+            window.location.href = '../info/index.html?cid=' + cityId + '&lat=' + lat + '&lng=' + lng;
+        }
+    }
+    else {
+        alert('获取位置失败，请确定已经获取当前位置！');
     }
 }
 
 function getLocation() {
+    $('#gps_city').text('定位中...');
     if ("geolocation" in navigator) {
         var options = {
-            enableHighAccuracy: false,
-            timeout: 6000,
+            enableHighAccuracy: true,
+            timeout: 10000,
             maximumAge: 0
         };
         navigator.geolocation.getCurrentPosition(success, error, options);
@@ -73,12 +74,16 @@ function getLocation() {
 
 function success(pos) {
     var crd = pos.coords;
-    var lat = crd.latitude;
-    var lng = crd.longitude;
-    $('#lct').val(lat + ';' + lng);
+    lat = crd.latitude;
+    lng = crd.longitude;
+    var url = 'https://api.chelaile.net.cn/goocity/city!localCity.action?s=IOS&gpsAccuracy=80.000000&gpstype=wgs&push_open=1&vc=10554&lat=' + lat + '&lng=' + lng;
+    var result = $.getApi(url, 'text');
+    cityInfo = JSON.parse(result.replace("**YGKJ", "").replace("YGKJ##", ""));
+    $('#gps_city').text(cityInfo.jsonr.data.localCity.cityProvince + cityInfo.jsonr.data.localCity.cityName);
 }
 
 function error(err) {
+    $('#gps_city').text('定位失败...');
     alert('获取当前地址失败,请开启定位功能或者手动搜索！' + err.message);
 }
 
